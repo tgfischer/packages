9842ef31ea9ce6bfd8ddde794bcd27e6f9518638
Tom Fischer <tgfischer6@gmail.com>
2019-03-17 14:21:32 -0400

    Initial commit

--- packages/client/scripts/destroy.js
+++
@@ -1,7 +0,0 @@
-const shell = require("shelljs");
-const minimist = require("minimist");
-
-const { stage = "prod" } = minimist(process.argv.slice(2));
-
-shell.exec(`npx serverless client remove --stage ${stage} --no-confirm`);
-shell.exec(`npx serverless remove --verbose --stage ${stage}`);
--- packages/client/src/hooks/useSidebar.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-
-import { SidebarContext } from "../contexts/SidebarProvider";
-
-export default () => {
-  const [isOpen, setIsOpen] = useContext(SidebarContext);
-  return [isOpen, () => setIsOpen(true), () => setIsOpen(false)];
-};
