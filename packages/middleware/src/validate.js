fcb4e2d6be296358517c5714dce50343dda1ee2c
Tom Fischer <tgfischer6@gmail.com>
2019-03-31 16:51:41 -0400

    Fixed module exports

--- packages/middleware/src/validate.js
+++ packages/middleware/src/validate.js
@@ -1,13 +1,7 @@
 const HttpStatus = require("http-status-codes");
 const asyncHandler = require("express-async-handler");
 
-module.exports.types = {
-  BODY: "body",
-  PARAMS: "params",
-  QUERY: "query"
-};
-
-module.exports.validate = (schema, type) =>
+module.exports = (schema, type) =>
   asyncHandler(async (req, res, next) => {
     try {
       const result = await schema.validate(req[type]);
